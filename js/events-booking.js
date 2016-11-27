aff0ae4e74f300452e78f63c6fca5056bd7a0185
samliweisen <liweisen007@gmail.com>
2016-11-27 22:30:16 +0000

    jquery script added

--- js/events-booking.js
+++ js/events-booking.js
@@ -1,6 +1,6 @@
 var events = [
     {
-        name: 'Sam\'s birthday',
+        name: 'Kid birthday',
         date: '2016-11-12',
         startTime: '2:00pm',
         endTime: '6:00pm',
@@ -16,15 +16,15 @@ var events = [
         photo: 'https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F26059567%2F66713630167%2F1%2Foriginal.jpg?h=140&w=280&rect=0%2C110%2C1346%2C673&s=53bb205cc3eed1d38c934098df7171e9',
     },
     {
-        name: 'Sam\'s birthday',
-        date: '2016-11-12',
+        name: 'Exam done celebration',
+        date: '2016-12-12',
         startTime: '2:00pm',
         endTime: '6:00pm',
         description: 'Event description',
         photo: 'https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F26059567%2F66713630167%2F1%2Foriginal.jpg?h=140&w=280&rect=0%2C110%2C1346%2C673&s=53bb205cc3eed1d38c934098df7171e9',
     },
     {
-        name: 'Team project celebration',
+        name: 'Digital design Final Project celebration',
         date: '2016-12-15',
         startTime: '2:00pm',
         endTime: '6:00pm',
@@ -33,15 +33,15 @@ var events = [
     },
     {
         name: 'Sam\'s birthday',
-        date: '2016-11-12',
+        date: '2016-12-16',
         startTime: '2:00pm',
         endTime: '6:00pm',
         description: 'Event description',
         photo: 'https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F26059567%2F66713630167%2F1%2Foriginal.jpg?h=140&w=280&rect=0%2C110%2C1346%2C673&s=53bb205cc3eed1d38c934098df7171e9',
     },
     {
-        name: 'Team project celebration',
-        date: '2016-12-15',
+        name: 'Christmax Party',
+        date: '2016-12-25',
         startTime: '2:00pm',
         endTime: '6:00pm',
         description: 'Event description',
@@ -50,17 +50,22 @@ var events = [
 ];
 
 window.onload = function() {
-    var eventsList = document.getElementById('events-list');
-    eventsList.innerHTML = renderEvents(events);
     
-    var eventForm = document.forms.eventform;
+    $('#events-list').html(renderEvents(events));
     
-    eventForm.onsubmit = function() {
-        var name = eventForm.name.value;
-        console.log(name);
-        return false;
-    }
+    $('#book-event').click(function() {
+        $('#events-list').hide('slow');
+        $('#eventform').show('slow');
+        $('#book-event').hide('slow');
+        $('#back-to-list').show('slow');
+    });
     
+    $('#back-to-list').click(function() {
+        $('#events-list').show('slow');
+        $('#eventform').hide('slow');
+        $('#back-to-list').hide('slow');
+        $('#book-event').show('slow');
+    });
 }
 
 function renderEvents(events) {
@@ -73,8 +78,12 @@ function renderEvents(events) {
 }
 
 function renderEvent(event) {
+    var date = new Date();
+    var format = date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate();
+    var expird = (format > event.date) ? '<div class="label">expird</div>' : '';
     return '<div class="event col">' +
                 '<div class="event-image">' +
+                    expird +
                     '<img src="' + event.photo + '" />' +
                 '</div>' +
                 '<div class="event-info">' +
